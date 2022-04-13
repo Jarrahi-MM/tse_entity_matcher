751a184504b7dfecc53dd5241ca5d0dc603bba97
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-04-13 16:55:08 +0430

    .

--- t1.py
+++ t1.py
@@ -1,24 +1,27 @@
 # %%
 import json
+import re
+from typing import Dict, List, Set
 
 # %%
-from typing import Dict, List
 
 bourseview_symbols_details = json.load(open('./bourseview_symbols_details.json'))
 # %%
-map_symbol_names: Dict[List] = {}
+map_symbol_names: Dict[str, Set[str]] = {}
 
 
 # %%
 def expand(name: str) -> str:
     name = name.replace('.', ' ')
+    name = name.strip()
+    name = re.sub('( |‌)+', '( |‌)', name)
     return name
 
 
 # %%
 for item in bourseview_symbols_details['items']:
     if item['symbolPouyaFa'] not in map_symbol_names:
-        map_symbol_names[item['symbolPouyaFa']] = []
-    map_symbol_names[item['symbolPouyaFa']].append(expand(item['isin']))
-    map_symbol_names[item['symbolPouyaFa']].append(expand(item['namePouya']))
-    map_symbol_names[item['symbolPouyaFa']].append(expand(item['namePouyaFa']))
+        map_symbol_names[item['symbolPouyaFa']] = set()
+    map_symbol_names[item['symbolPouyaFa']].add(expand(item['isin']))
+    map_symbol_names[item['symbolPouyaFa']].add(expand(item['namePouya']))
+    map_symbol_names[item['symbolPouyaFa']].add(expand(item['namePouyaFa']))
