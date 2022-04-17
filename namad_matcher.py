59a9665852d68dfcbb25610f157ad4b8124cf0e6
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-04-17 11:12:47 +0430

    add tag_numbers (coauthor: negin jafari)

--- namad_matcher.py
+++ namad_matcher.py
@@ -5,14 +5,18 @@ from typing import Dict, List, Set
 from parsi_io.modules.number_extractor import NumberExtractor
 
 # %%
-bourseview_symbols_details = json.load(open('./bourseview_symbols_details.json'))
+bourseview_symbols_details = json.load(
+    open('./bourseview_symbols_details.json'))
 map_symbol_names: Dict[str, Set[str]] = {}
 
 # %%
+
+
 def expand_name(name: str) -> str:
     name = name.strip()
     name = re.sub(r'( |‌|\(|\)|-|_|ـ|\.)+', r'(| |‌|\(|\)|-|_|ـ|\.)+', name)
     return name
+
 
 def expand_term(term: str) -> str:
     term = re.sub(r'#عدد', r'~+', term)
@@ -21,15 +25,27 @@ def expand_term(term: str) -> str:
     term = re.sub(r'( |‌|\(|\)|-|_|ـ|\.)+', r'(| |‌|\(|\)|-|_|ـ|\.)+', term)
     return term
 
+
+def tag_numbers(original_text: str) -> str:
+    extractor = NumberExtractor()
+    new_text = original_text
+    num_list = extractor.run(original_text)
+    for d in num_list:
+        len = d["span"][1] - d["span"][0]
+        new_text = new_text[:d["span"][0]]+'~'*(len)+new_text[d["span"][1]:]
+    return new_text
+
+
 # %%
 for item in bourseview_symbols_details['items']:
     if item['symbolPouyaFa'] not in map_symbol_names:
         map_symbol_names[item['symbolPouyaFa']] = set()
     map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['isin']))
     map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['namePouya']))
-    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['namePouyaFa']))
+    map_symbol_names[item['symbolPouyaFa']].add(
+        expand_name(item['namePouyaFa']))
 
-#%%
+# %%
 # regex = expand('نفت ( ) -- _ـ  ‌ ها')
 # test_str = 'نفتها'
 
@@ -37,4 +53,3 @@ for item in bourseview_symbols_details['items']:
 
 # for matchNum, match in enumerate(matches, start=1):
 #     print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
