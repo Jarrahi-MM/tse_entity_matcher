2f9c4b00884af639285b2ec68c3a0bec9bdc1411
jafarinegin <neginjfi79@gmail.com>
2022-04-17 12:43:04 +0430

    Add expand_term & create event dict Add unit tests

--- namad_matcher.py
+++ namad_matcher.py
@@ -1,7 +1,7 @@
 # %%
 import json
 import re
-from typing import Dict, List, Set
+from typing import Dict, List
 from parsi_io.modules.number_extractor import NumberExtractor
 
 # %%
@@ -19,8 +19,8 @@ def expand_name(name: str) -> str:
 
 
 def expand_term(term: str) -> str:
-    term = re.sub(r'#عدد', r'~+', term)
-    term = re.sub(r'؟', r'\?', term)
+    term = re.sub(r'#NUM', r'(~+)', term)
+    term = re.sub(r'؟', r'?', term)
     term = term.strip()
     term = re.sub(r'( |‌|\(|\)|-|_|ـ|\.)+', r'(| |‌|\(|\)|-|_|ـ|\.)+', term)
     return term
@@ -39,11 +39,16 @@ def tag_numbers(original_text: str) -> str:
 # %%
 for item in bourseview_symbols_details['items']:
     if item['symbolPouyaFa'] not in map_symbol_names:
-        map_symbol_names[item['symbolPouyaFa']] = set()
-    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['isin']))
-    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['namePouya']))
-    map_symbol_names[item['symbolPouyaFa']].add(
+        map_symbol_names[item['symbolPouyaFa']] = []
+    map_symbol_names[item['symbolPouyaFa']].append(expand_name(item['isin']))
+    map_symbol_names[item['symbolPouyaFa']].append(expand_name(item['namePouya']))
+    map_symbol_names[item['symbolPouyaFa']].append(
         expand_name(item['namePouyaFa']))
+# %%
+for index, row in events_df.iterrows():
+    if row['نوع اصطلاح'] not in events:
+        events[row['نوع اصطلاح']] = []
+    events[row['نوع اصطلاح']].append(expand_term(row['رجکس اصلاح']))
 
 # %%
 # regex = expand('نفت ( ) -- _ـ  ‌ ها')
--- test_namad_matcher.py
+++ test_namad_matcher.py
@@ -1,6 +1,7 @@
 import unittest
-from namad_matcher import expand_name, tag_numbers
+from namad_matcher import expand_name, tag_numbers, expand_term
 import re
+from namad_matcher import events
 
 
 class TestStringMethods(unittest.TestCase):
@@ -18,6 +19,48 @@ class TestStringMethods(unittest.TestCase):
         original_text = 'امسال سه درصد یا ۳.۱۳ درصد یا 3.13 درصد سود کسب کردم.'
         true_tagged_text = 'امسال ~~ درصد یا ~~~~ درصد یا ~~~~ درصد سود کسب کردم.'
         self.assertTrue(tag_numbers(original_text) == true_tagged_text)
+
+    def test_expand_term_1(self):
+        original_text = 'جریان آغاز معاملات فزر با ۱۵.۲ واحد تاثیر مثبت بر روند صعودی بازار فرابورس اثر گذار بود.'
+        tagged_text = tag_numbers(original_text)
+        regex = events['رشد سهم'][0]
+        matches = re.finditer(regex, tagged_text, re.MULTILINE)
+        matches_list = []
+        for matchNum, match in enumerate(matches, start=1):
+            matches_list.append(original_text[match.start(): match.end()])
+        self.assertTrue(len(matches_list) == 1)
+        self.assertTrue(matches_list[0] == '۱۵.۲ واحد تاثیر مثبت')
+
+    def test_expand_term_2(self):
+        # Todo سهم پاک شد، کد کتابخانه مشکل دارد
+        original_text = 'برکت همین افشای ب باعث شد سه درصد مثبت شود. به خاطر همین میگم پیگیر باشید.'
+        tagged_text = tag_numbers(original_text)
+        regex = events['رشد سهم'][0]
+        matches = re.finditer(regex, tagged_text, re.MULTILINE)
+        matches_list = []
+        for matchNum, match in enumerate(matches, start=1):
+            matches_list.append(original_text[match.start(): match.end()])
+        self.assertTrue(len(matches_list) == 1)
+        self.assertTrue(matches_list[0] == 'سه درصد مثبت')
+
+        regex = events['اطلاعیه'][0]
+        matches = re.finditer(regex, tagged_text, re.MULTILINE)
+        matches_list = []
+        for matchNum, match in enumerate(matches, start=1):
+            matches_list.append(original_text[match.start(): match.end()])
+        self.assertTrue(len(matches_list) == 1)
+        self.assertTrue(matches_list[0] == 'افشای ب')
+
+    def test_expand_term_3(self):
+        original_text = 'گزارش فعالیت ماهانه دوره ۱ ماهه منتهی به ۱۴۰۰/۰۹/۳۰ برای دیران منتشر شد.'
+        tagged_text = tag_numbers(original_text)
+        regex = events['گزارش'][0]
+        matches = re.finditer(regex, tagged_text, re.MULTILINE)
+        matches_list = []
+        for matchNum, match in enumerate(matches, start=1):
+            matches_list.append(original_text[match.start(): match.end()])
+        self.assertTrue(len(matches_list) == 1)
+        self.assertTrue(matches_list[0] == 'گزارش فعالیت ماهانه')
 
 
 if __name__ == '__main__':
