1ecada9f8feb37b47ef0c8403984b5678d4bbb2e
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-04-17 10:21:26 +0430

    add tag_numbers (author: negin jafari)

--- namad_matcher.py
+++ namad_matcher.py
@@ -2,25 +2,32 @@
 import json
 import re
 from typing import Dict, List, Set
+from parsi_io.modules.number_extractor import NumberExtractor
 
 # %%
 bourseview_symbols_details = json.load(open('./bourseview_symbols_details.json'))
 map_symbol_names: Dict[str, Set[str]] = {}
 
 # %%
-def expand(name: str) -> str:
+def expand_name(name: str) -> str:
     name = name.strip()
     name = re.sub(r'( |‌|\(|\)|-|_|ـ|\.)+', r'(| |‌|\(|\)|-|_|ـ|\.)+', name)
     return name
 
+def expand_term(term: str) -> str:
+    term = re.sub(r'#عدد', r'~+', term)
+    term = re.sub(r'؟', r'\?', term)
+    term = term.strip()
+    term = re.sub(r'( |‌|\(|\)|-|_|ـ|\.)+', r'(| |‌|\(|\)|-|_|ـ|\.)+', term)
+    return term
 
 # %%
 for item in bourseview_symbols_details['items']:
     if item['symbolPouyaFa'] not in map_symbol_names:
         map_symbol_names[item['symbolPouyaFa']] = set()
-    map_symbol_names[item['symbolPouyaFa']].add(expand(item['isin']))
-    map_symbol_names[item['symbolPouyaFa']].add(expand(item['namePouya']))
-    map_symbol_names[item['symbolPouyaFa']].add(expand(item['namePouyaFa']))
+    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['isin']))
+    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['namePouya']))
+    map_symbol_names[item['symbolPouyaFa']].add(expand_name(item['namePouyaFa']))
 
 #%%
 # regex = expand('نفت ( ) -- _ـ  ‌ ها')
--- test_namad_matcher.py
+++ test_namad_matcher.py
@@ -1,18 +1,23 @@
 import unittest
-from namad_matcher import expand
+from namad_matcher import expand_name, tag_numbers
 import re
 
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        regex = expand('نفت ( ) -- _ـ  ‌ ها')
+    def test_expand_name(self):
+        regex = expand_name('نفت ( ) -- _ـ  ‌ ها')
         test_str = 'نفتها'
         matches = re.finditer(regex, test_str, re.MULTILINE)
         matches_count = 0
         for _, _ in enumerate(matches, start=1):
             matches_count += 1
         self.assertTrue(matches_count == 1)
+
+    def test_tag_numbers(self):
+        original_text = 'امسال سه درصد یا ۳.۱۳ درصد یا 3.13 درصد سود کسب کردم.'
+        true_tagged_text = 'امسال ~~ درصد یا ~~~~ درصد یا ~~~~ درصد سود کسب کردم.'
+        self.assertTrue(tag_numbers(original_text) == true_tagged_text)
 
 
 if __name__ == '__main__':
